2d3caf761dfdfe85bc12fb6ba2887202f7082021
also easy <1261396459@qq.com>
2025-09-13 20:23:32 +0800

    add module Bigraph

---
+++ Bigraph.py
@@ -0,0 +1,14 @@
+# @AlaoCode#> This module places the graph structure of DGEKT.
+
+class BiGraph:
+    '''
+    The bipartite graph object of q2s.
+    Due to the use of independent modeling, the number of nodes for q and s has been doubled.
+    '''
+    def __init__(self, q_size, s_size):
+        pass
+    def add_edges_from_structured_seqs(self, seqs):
+        '''
+        Add edges to the graph from structured sequences.
+        '''
+        pass
