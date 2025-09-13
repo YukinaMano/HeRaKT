8e3deb6380fd18e8efcfad60b06fc28539ecbbb2
also easy <1261396459@qq.com>
2025-09-13 20:23:46 +0800

    add module Q2Sgraph

---
+++ Q2Sgraph.py
@@ -0,0 +1,13 @@
+# @AlaoCode#> This module places universal graph structure.
+
+class Q2SGraph:
+    '''
+    The bipartite graph object of q2s.
+    '''
+    def __init__(self, q_size, s_size):
+        pass
+    def add_edges_from_structured_seqs(self, seqs):
+        '''
+        Add edges to the graph from structured sequences.
+        '''
+        pass
