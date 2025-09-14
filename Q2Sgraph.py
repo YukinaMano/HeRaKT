3e4e6d6c417a0a01cc18eacf21816d9106b40a9e
also easy <1261396459@qq.com>
2025-09-14 19:58:57 +0800

    add model HeRaKT and its Graph

--- Q2Sgraph.py
+++ Q2Sgraph.py
@@ -1,13 +1,144 @@
 # @AlaoCode#> This module places universal graph structure.
+from collections import Counter
+from utils._common import print_debug
 
 class Q2SGraph:
     '''
     The bipartite graph object of q2s.
     '''
     def __init__(self, q_size, s_size):
-        pass
-    def add_edges_from_structured_seqs(self, seqs):
+        super(Q2SGraph, self).__init__()
+        self.q_size = q_size
+        self.s_size = s_size
+
+        self.edges_lis = []
+        self.q_nodes_set = Counter()
+        self.s_nodes_set = Counter()
+        self.edges_set = Counter()
+        self.no_load_data = True
+        self.no_build_graph = True
+
+    def add_edges_from_seqs(self, seqs_lis):
         '''
-        Add edges to the graph from structured sequences.
+        Storing graphs with edge weights.
+        Args:
+            seqs_lis(list): A list of sequences, each sequence is a list of triples (s, q, a).
         '''
+        if seqs_lis == []:
+            return
+        self.no_load_data = False
+        edges_lis = self.edges_lis
+        q_nodes_set = self.q_nodes_set
+        s_nodes_set = self.s_nodes_set
+
+        for seq in seqs_lis:
+            for s, q, a in seq:
+                edges_lis.append((q, s))
+                q_nodes_set[q] += 1
+                s_nodes_set[s] += 1
+
+        self.edges_lis = edges_lis
+        self.q_nodes_set = q_nodes_set
+        self.s_nodes_set = s_nodes_set
+        return self
+
+    def get_q2s_sparse_adj_matrix(self):
+        '''
+        Obtain the sparse adjacency matrix of the Q2S graph.
+        Returns:
+            tuple:
+                - q2s_edge_lis(list{2, Edges}): The edge list of Q2S graph
+                - nz_s_weight(list{Edges}): The weight of Q2S graph edges
+        '''
+        if self.no_build_graph:
+            self.build_graph()
+            self.no_build_graph = False
+
+        edges_set = self.edges_set
+        q2s_edge_lis = []
+        weight_lis = []
+        s_weight_lis = [0 for _ in range(self.s_size[-1] + 1)]
+        for (q, s), weight in edges_set.items():
+            q2s_edge_lis.append([q, s])
+            weight_lis.append(weight)
+            s_weight_lis[s] += weight
+            
+        nz_s_weight = [weight_lis[i] / s_weight_lis[q2s_edge_lis[i][1]] for i in range(len(q2s_edge_lis))] 
+        return q2s_edge_lis, nz_s_weight
+
+    def get_q2s_adj_list(self):
+        '''
+        Obtain the q2s adjacency table.
+        Returns:
+            tuple:
+                - q2s_adj_lis(list{Q, list{Neighbors}}): The adjacency list of Q2S graph
+                - s2q_adj_lis(list{S, list{Neighbors}}): The adjacency list of S2Q graph
+        '''
+        if self.no_build_graph:
+            self.build_graph()
+            self.no_build_graph = False
+
+        edges_set = self.edges_set
+        q_size = self.q_size[-1] + 1
+        s_size = self.s_size[-1] + 1
+        q2s_adj_lis = [[] for _ in range(q_size)]
+        s2q_adj_lis = [[] for _ in range(s_size)]
+        for (q, s), weight in edges_set.items():
+            q2s_adj_lis[q].append(s)
+            s2q_adj_lis[s].append(q)
+        q_neighbors_num = [len(q_neighbors) for q_neighbors in q2s_adj_lis]
+        s_neighbors_num = [len(s_neighbors) for s_neighbors in s2q_adj_lis]
+
+        print_debug('\n\n>>>[Complete] Adjacency information export')
+        print_debug(f'Questions/Skills Node Number is: {len(q2s_adj_lis)}/{len(s2q_adj_lis)}')
+        print_debug(f'Questions/Skills Max of Number-Neighbors is: {max(q_neighbors_num)}/{max(s_neighbors_num)}')
+        return q2s_adj_lis, s2q_adj_lis
+
+    def build_graph(self):
+        '''
+        Build the graph by edges and weights.
+        '''
+        if self.no_load_data:
+            print('[Error] The data in the picture has not been loaded yet, stop running.')
+            return
+        edges_lis = self.edges_lis
+        edges_set = Counter(edges_lis)
+        q_nodes_set = self.q_nodes_set
+        s_nodes_set = self.s_nodes_set
+
+        print_debug('\n\n>>>[Complete] Graph building')
+        print_debug(f'Valid Edge Number is: {len(edges_set)}')
+        print_debug(f'Valid Question Node Number is: {len(q_nodes_set)}')
+        print_debug(f'Valid Skill Node Number is: {len(s_nodes_set)}')
+        self.edges_set = edges_set
+        self.no_build_graph = False
+        return self
+
+    def add_edges_from_structured_seqs(self, seqs_lis):
+        '''
+        Add edges through structured sequences.
+        Args:
+            seqs_lis(list): A list of sequences, each sequence is a list of triples (s_, q_, a_).
+                s_(list): A list of skills
+                q_(list): A list of questions
+                a_(list): A list of answers
+        '''
+        if seqs_lis == []:
+            return
+        self.no_load_data = False
+        edges_lis = self.edges_lis
+        q_nodes_set = self.q_nodes_set
+        s_nodes_set = self.s_nodes_set
+
+        for seq in seqs_lis:
+            for s_, q_, a_ in seq:
+                q_nodes_set.update(q_)
+                s_nodes_set.update(s_)
+                q = q_[0]
+                for s in s_:
+                    edges_lis.append((q, s))
+
+        self.edges_lis = edges_lis
+        self.q_nodes_set = q_nodes_set
+        self.s_nodes_set = s_nodes_set
         return self
--- model/herakt.py
+++ model/herakt.py
@@ -2,9 +2,279 @@
 import torch
 import torch.nn as nn
 from model._template_model import TrainModel
-from model.dkt import DKT
 
-class HeRaKT(DKT):
+class HeRaKT(TrainModel):
     def __init__(self, args, model_name='HeRaKT'):
         super(HeRaKT, self).__init__(args, model_name)
-        pass
+        # Data control
+        # where data control changed as dataset
+        self.zero2zero = True
+        self.q_range = args.q_range
+        self.s_range = args.s_range
+        self.a_range = args.a_range
+        # Model architecture hyperparameters
+        self.q_sparsity = args.q_sparsity
+        self.s_sparsity = args.s_sparsity
+        self.UNKNOWN_ID = 3
+        self.embedding_size = args.embedding_size
+        self.pos_l = args.pos_l
+        self.neg_l = args.neg_l
+        self.rnn_l = args.rnn_l
+        self.dropout = args.dropout
+        self.padding_idx = args.padding_idx
+        self.max_step = args.max_step
+        self.train_strategy = 'MASK_LEARNING'
+        # Model architecture
+        # Embedding layer
+        self.question_embedding_layer = SemanticEmbedding(self.q_range, self.embedding_size, self.q_sparsity, padding_idx=self.padding_idx)
+        self.skill_embedding_layer = SemanticEmbedding(self.s_range, self.embedding_size, self.s_sparsity, padding_idx=self.padding_idx)
+        self.answer_embedding_layer = nn.Embedding(self.a_range, self.embedding_size, padding_idx=self.padding_idx)
+        self.avg_gcn_layer = AvgGCN(self.question_embedding_layer, self.skill_embedding_layer, layer_num=1, zero2zero=True)
+        # Enhancer
+        self.enhancer = BiLSTM(
+            self.embedding_size, 
+            self.embedding_size,
+            num_layers=self.pos_l,
+            batch_first=True
+        )
+        # Denoiser
+        self.denoiser = LFFTStack(seq_len=self.max_step, d_model=self.embedding_size, n_layers=self.neg_l, channelwise=False)
+        # Propagation layer
+        self.lstm = nn.LSTM(self.embedding_size, self.embedding_size, self.rnn_l, dropout=self.dropout, batch_first=True)
+        # Prediction layer
+        self.biline_predict_layer = nn.Bilinear(self.embedding_size, self.embedding_size, 1)
+        # FC units
+        self.pos_fc = nn.Linear(self.embedding_size*3, self.embedding_size)
+        self.neg_fc = nn.Linear(self.embedding_size*3, self.embedding_size)
+        self.row_fc = nn.Linear(self.embedding_size*3, self.embedding_size)
+        # Norm&Add
+        self.norm_layer = nn.LayerNorm(self.embedding_size)
+        self.dropout_layer = nn.Dropout(self.dropout)
+        # Nonlinear unit
+        self.relu = nn.ReLU()
+        self.sigmoid = nn.Sigmoid()
+        # loss - BCEWithLogit
+        self.loss_fn = nn.BCEWithLogitsLoss()
+
+    def forward(self, seqs):
+        s = seqs[:, :, 0]
+        q = seqs[:, :, 1]
+        a = seqs[:, :, 2]
+        # Emb
+        s_embedding = self.avg_gcn_layer(q, self.sparse_graph)
+        q_embedding = self.question_embedding_layer(q)
+        a_embedding = self.answer_embedding_layer(a)
+        # Correctness assumption
+        t = a.clone()
+        t[a != 0] = 2
+        t_embedding = self.answer_embedding_layer(t)
+        # Heterogeneous modeling
+        e_pos = self.relu(self.pos_fc(torch.cat((q_embedding, s_embedding, a_embedding), dim=-1)))
+        e_neg = self.relu(self.neg_fc(torch.cat((q_embedding, s_embedding, a_embedding), dim=-1)))
+        e_row = self.relu(self.row_fc(torch.cat((q_embedding, s_embedding, a_embedding), dim=-1)))
+        e_pos_enh = self.enhancer(e_pos)
+        e_neg_enh = self.denoiser(e_neg)
+        f_mask = (a == self.id_offset).float().unsqueeze(-1)
+        t_mask = (a == (self.id_offset + 1)).float().unsqueeze(-1)
+        e_embedding = self.norm_layer(self.dropout_layer(e_pos_enh) * t_mask + self.dropout_layer(e_neg_enh) * f_mask + e_row)
+        r_embedding = self.relu(self.pos_fc(torch.cat((q_embedding, s_embedding, t_embedding), dim=-1)))
+        # Rnn
+        outputs, _ = self.lstm(e_embedding)
+        # Output P{h_t => r_t}
+        h_t = outputs[:, :-1, :]
+        r_t = r_embedding[:, 1:, :]
+        logits = self.biline_predict_layer(h_t, r_t)[:, :, -1]
+        return logits
+
+    def set_graph_adj(self, graph_info_func):
+        '''
+        Get global Q2S graph.
+        '''
+        sparse_graph, _, _ = graph_info_func.get_global_graph()
+        self.sparse_graph = sparse_graph.to(self.device)
+
+class BiLSTM(nn.Module):
+    def __init__(self, embedding_size, hidden_size, num_layers=1, dropout=0.0, batch_first=True, bidirectional=True):
+        '''
+        The BiLSTM module to enhance positive embeddings.
+        Args:
+            num_layers(int): The number of layers
+            dropout(float): The dropout rate
+            batch_first(bool): Whether the input tensor is in batch first format
+            bidirectional(bool): Whether the LSTM is bidirectional
+        Inputs:
+            x{B, S, D}: Input tensor to be enhanced
+        Outputs:
+            output{B, S, D}: The enhanced output tensor
+        '''
+        super(BiLSTM, self).__init__()
+        self.lstm = nn.LSTM(embedding_size, hidden_size, num_layers, dropout=dropout, bidirectional=bidirectional, batch_first=batch_first)
+        self.hidden_size = hidden_size
+        self.direction = 2 if bidirectional else 1
+        self.fc = nn.Linear(hidden_size * self.direction, embedding_size)
+    def forward(self, x):
+        lstm_out, (h_n, c_n) = self.lstm(x)
+        out = self.fc(lstm_out)
+        return out
+
+class LFFTBlock(nn.Module):
+    def __init__(
+        self,
+        seq_len: int,
+        d_model: int,
+        channelwise: bool = False
+    ):
+        '''
+        The LFFT layer, FFT -> Learnable filtering -> iFFT.
+        Args:
+            seq_len(int): sequence length
+            d_model(int): The number of channels, equivalent to the embedding dimension
+            channelwise(bool): Whether to learn frequency weights per channel, 
+                learn (S, D) weights if True; learn (S, 1) weights if False, share weights across channels
+        Inputs:
+            x{B, S, D}: Input tensor to be denoised
+        Outputs:
+            output{B, S, D}: The denoised output tensor
+        '''
+        super().__init__()
+        self.seq_len = seq_len
+        self.d_model = d_model
+        self.channelwise = channelwise
+        if channelwise:
+            self.filter_weights = nn.Parameter(torch.ones(seq_len, d_model))
+        else:
+            self.filter_weights = nn.Parameter(torch.ones(seq_len, 1))
+        self.act = nn.Sigmoid()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, S, D = x.shape
+        assert S == self.seq_len and D == self.d_model, \
+            f"Input shape (S={S}, D={D}) must match configured (S={self.seq_len}, D={self.d_model})."
+        x_freq = torch.fft.fft(x, dim=1)    # FFT
+        W = self.act(self.filter_weights)   # Learnable filtering
+        W = W.unsqueeze(0)                  # -> (1, S, 1) or (1, S, D)
+        x_freq_filt = x_freq * W            # Take the actual part
+        x_denoised = torch.fft.ifft(x_freq_filt, dim=1).real    # IFFT
+        return x_denoised
+
+
+class LFFTStack(nn.Module):
+    def __init__(
+        self,
+        seq_len: int,
+        d_model: int,
+        n_layers: int = 1,
+        channelwise: bool = False,
+        share_weights: bool = False
+    ):
+        '''
+        The LFFTStack module to denoise negative embeddings.
+        Args:
+            seq_len(int): sequence length
+            d_model(int): The number of channels, equivalent to the embedding dimension
+            n_layers(int): The number of LFFT layers
+            channelwise(bool): Whether to learn frequency weights per channel, 
+                learn {S, D} weights if True; learn {S, 1} weights if False, share weights across channels
+            share_weights(bool): Whether to share weights across layers
+        Inputs:
+            x{B, S, D}: Input tensor to be denoised
+        Outputs:
+            output{B, S, D}: The denoised output tensor
+        '''
+        super().__init__()
+        self.seq_len = seq_len
+        self.d_model = d_model
+        self.n_layers = n_layers
+
+        if share_weights:
+            block = LFFTBlock(seq_len, d_model, channelwise)
+            self.blocks = nn.ModuleList([block] + [block for _ in range(n_layers - 1)])
+        else:
+            self.blocks = nn.ModuleList([
+                LFFTBlock(seq_len, d_model, channelwise)
+                for _ in range(n_layers)
+            ])
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        for blk in self.blocks:
+            x = blk(x)
+        return x
+
+class SemanticEmbedding(nn.Module):
+    def __init__(self, num_embeddings: int, embedding_dim: int, t=0.5, padding_idx=None, **kwargs):
+        '''
+        Enhance the embedding layer using heuristic methods, initialize the embedding with soft one-hot.
+        Args:
+            t(float): Sparsity threshold, values below t are set to 0, values above t are kept
+        Inputs:
+            x{B, S}: Input question ID sequences
+        Outputs:
+            output{B, S, D}: The embedding tensor
+        '''
+        super(SemanticEmbedding, self).__init__()
+        self.num_embeddings = num_embeddings
+        self.embedding_dim = embedding_dim
+        self.threshold = t
+        self.tau = 1 - t
+
+        init_weight = torch.randn(num_embeddings, embedding_dim)  # (Q or S, D)
+        # # Gumbel Softmax generates sparse representations
+        # sparse_weight = F.gumbel_softmax(init_weight, tau=self.tau, hard=False)
+        # Thresholding generates sparse representations
+        init_weight = torch.abs(init_weight)
+        mask =  torch.abs(init_weight) >= self.threshold 
+        sparse_weight = init_weight * mask
+        self.weight = nn.Parameter(sparse_weight)
+
+    def forward(self, x):
+        return self.weight[x]
+
+
+class AvgGCN(nn.Module):
+    # input_size:   {batch, nodes_num}
+    # output_size:  {batch, nodes_num, embedding_size}
+    def __init__(self, q_embedding_layer: nn.Embedding, s_embedding_layer: nn.Embedding, layer_num=1, zero2zero=False):
+        '''
+        Average convolutional neighbor vector.
+        Args:
+            q_embedding_layer(nn.Embedding): The query embedding layer
+            s_embedding_layer(nn.Embedding): The semantic embedding layer
+            layer_num(int): The number of layers
+            zero2zero(bool): Whether to set the embedding of padding token to 0
+        Inputs:
+            x{B, N}: Input question ID sequences
+        Outputs:
+            output{B, N, D}: The output tensor
+        '''
+        super(AvgGCN, self).__init__()
+        self.q_embedding_layer = q_embedding_layer
+        self.s_embedding_layer = s_embedding_layer
+        self.layer_num = layer_num
+        self.zero2zero = zero2zero
+        self.gcn_q_Ematrix = None
+        self.gcn_s_Ematrix = None
+
+    def forward(self, x: torch.Tensor, q2s_A_matrix: torch.sparse.Tensor):
+        q_Ematrix = self.q_embedding_layer.weight
+        s_Ematrix = self.s_embedding_layer.weight
+        # Compute the degree of d_q and d_s
+        d_q = torch.sparse.sum(q2s_A_matrix, dim=1).to_dense().clamp(min=1)
+        d_s = torch.sparse.sum(q2s_A_matrix, dim=0).to_dense().clamp(min=1)
+        # Compute normalization factor ω=1/d
+        omega_q = 1 / d_q.unsqueeze(1)
+        omega_s = 1 / d_s.unsqueeze(1)        
+        for _ in range(self.layer_num):
+            # Compute E'=A'*X
+            l_q_Ematrix = torch.sparse.mm(q2s_A_matrix, s_Ematrix)
+            l_s_Ematrix = torch.sparse.mm(q2s_A_matrix.t(), q_Ematrix)
+            # Compute E=ωE'
+            q_Ematrix = l_q_Ematrix * omega_q
+            s_Ematrix = l_s_Ematrix * omega_s
+
+        self.gcn_q_Ematrix = q_Ematrix
+        self.gcn_s_Ematrix = s_Ematrix
+        x_embedding = self.gcn_q_Ematrix[x]
+        if self.zero2zero:
+            mask = (x == 0).unsqueeze(-1).to(torch.float32)
+            x_embedding = (1 - mask) * x_embedding
+        return x_embedding
